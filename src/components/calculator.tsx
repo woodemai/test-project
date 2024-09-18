f81f989051f1446ed79429adb7b2be969ba3cc5f
Nikolay Savchenko <n.savchenkoo73@gmail.com>
2024-09-18 10:16:25 +0300

    update calculator

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -1,102 +1,98 @@
 "use client";
 import { useState } from "react";
-import CalculatorButton, {
-  CalculatorButtonProps,
-} from "./calculator/calc-button";
-
-const buttons: CalculatorButtonProps[] = [
-  {
-    children: "C",
-    color: "calcSecondary",
-  },
-  {
-    children: "±",
-    color: "calcSecondary",
-  },
-  {
-    children: "%",
-    color: "calcSecondary",
-  },
-  {
-    children: "÷",
-    color: "calcPrimary",
-  },
-  {
-    children: "7",
-  },
-  {
-    children: "8",
-  },
-  {
-    children: "9",
-  },
-  {
-    children: "×",
-    color: "calcPrimary",
-  },
-  {
-    children: "4",
-  },
-  {
-    children: "5",
-  },
-  {
-    children: "6",
-  },
-  {
-    children: "-",
-    color: "calcPrimary",
-  },
-  {
-    children: "1",
-  },
-  {
-    children: "2",
-  },
-  {
-    children: "3",
-  },
-  {
-    children: "+",
-    color: "calcPrimary",
-  },
-  {
-    children: ".",
-  },
-  {
-    children: "0",
-  },
-  {
-    children: "⌫",
-  },
-  {
-    children: "=",
-    color: "calcPrimary",
-  },
-];
+import CalculatorButton from "./calculator/calc-button";
 
 const Calculator = () => {
-  const [value, setValue] = useState("0");
-
-  const onClick = (newValue: string) => {
-    if(value === '0') {
-        setValue(newValue)
-    }else {
-        setValue(prev => `${prev} ${newValue}`)
-    }
-  }
+  const [value, setValue] = useState("");
 
   return (
     <div className="w-fit rounded-lg bg-gray-100 p-4 shadow-sm">
-      <div className="text-right w-full p-4 font-light text-5xl">
+      <div className="w-full min-w-20 p-4 text-right text-5xl font-light">
         <h2>{value}</h2>
       </div>
       <div className="grid w-fit grid-cols-4 gap-2">
-        {buttons.map(({ children, color }) => (
-          <CalculatorButton onClick={onClick} color={color} key={children?.toString()}>
-            {children}
-          </CalculatorButton>
-        ))}
+        <CalculatorButton onClick={() => setValue("")} color="calcSecondary">
+          C
+        </CalculatorButton>
+        <CalculatorButton
+          onClick={() => setValue((prev) => `-${prev}`)}
+          color="calcSecondary"
+        >
+          ±
+        </CalculatorButton>
+        <CalculatorButton
+          onClick={() => setValue((prev) => prev + "%")}
+          color="calcSecondary"
+        >
+          %
+        </CalculatorButton>
+        <CalculatorButton
+          onClick={() => setValue((prev) => prev + "/")}
+          color="calcPrimary"
+        >
+          ÷
+        </CalculatorButton>
+        <CalculatorButton onClick={() => setValue((prev) => prev + "7")}>
+          7
+        </CalculatorButton>
+        <CalculatorButton onClick={() => setValue((prev) => prev + "8")}>
+          8
+        </CalculatorButton>
+        <CalculatorButton onClick={() => setValue((prev) => prev + "9")}>
+          9
+        </CalculatorButton>
+        <CalculatorButton
+          onClick={() => setValue((prev) => prev + "*")}
+          color="calcPrimary"
+        >
+          ×
+        </CalculatorButton>
+        <CalculatorButton onClick={() => setValue((prev) => prev + "4")}>
+          4
+        </CalculatorButton>
+        <CalculatorButton onClick={() => setValue((prev) => prev + "5")}>
+          5
+        </CalculatorButton>
+        <CalculatorButton onClick={() => setValue((prev) => prev + "6")}>
+          6
+        </CalculatorButton>
+        <CalculatorButton
+          onClick={() => setValue((prev) => prev + "-")}
+          color="calcPrimary"
+        >
+          -
+        </CalculatorButton>
+        <CalculatorButton onClick={() => setValue((prev) => prev + "1")}>
+          1
+        </CalculatorButton>
+        <CalculatorButton onClick={() => setValue((prev) => prev + "2")}>
+          2
+        </CalculatorButton>
+        <CalculatorButton onClick={() => setValue((prev) => prev + "3")}>
+          3
+        </CalculatorButton>
+        <CalculatorButton
+          onClick={() => setValue((prev) => prev + "+")}
+          color="calcPrimary"
+        >
+          +
+        </CalculatorButton>
+        <CalculatorButton onClick={() => setValue((prev) => prev + ".")}>
+          .
+        </CalculatorButton>
+        <CalculatorButton onClick={() => setValue((prev) => prev + "0")}>
+          0
+        </CalculatorButton>
+        <CalculatorButton onClick={() => setValue((prev) => prev.slice(0, -1))}>
+          ⌫
+        </CalculatorButton>
+        <CalculatorButton
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+          onClick={() => setValue((prev) => eval(prev))}
+          color="calcPrimary"
+        >
+          =
+        </CalculatorButton>
       </div>
     </div>
   );
